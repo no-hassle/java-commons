57fd53c2e2864cb51ecf88fe3fabb90a8d4d86c8
Arne Georg Gleditsch <argggh@telenordigital.com>
2017-01-20 16:33:43 +0100

    [emjar-demo] Add demonstration of different emjar alternatives

---
+++ emjar-demo/src/main/java/com/comoyo/commons/emjar/demo/rs/Endpoint.java
@@ -0,0 +1,25 @@
+package com.comoyo.commons.emjar.demo.rs;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class Endpoint {
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String htmlHandler() {
+        return "<html><body><h1>OK</h1></body></html>";
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Map<String,String> jsonHandler() {
+        final Map<String,String> map = new HashMap<>();
+        map.put("key", "value");
+        return map;
+    }
+}
